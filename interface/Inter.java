a1728b838c65ff9eb6da916db5b0334749fafa1b
Wilkias Freitas <114428486+Dev-Wil97@users.noreply.github.com>
2023-06-27 19:57:19 -0300

    Add files via upload

---
+++ interface/Inter.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.inter;
+
+
+public class Inter {
+
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        
+  }
+}
